de8166a6ff2c30fd998218a75a38d69eeb0bac47
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2022-09-16 14:22:16 +0530

    Create jwt.js

---
+++ js/jwt.js
@@ -0,0 +1,42 @@
+// Import the axios HTTP library and the SDK methods you'll be calling
+import axios from 'axios';
+import {
+ initialize,
+ getInAppMessages,
+ updateUser,
+ track
+} from '@iterable/web-sdk';
+(() => {
+ const yourAsyncJWTGeneratorMethod = ({ email }) => {
+ // In this method, call your server to fetch a valid JWT token for the current
+ // user. Pass along the relevant userId or email, so the server can include
+ // use it to generate the token. This method should return the JWT token.
+ };
+ // If you use userId instead of email, import setUserId instead
+ const { setEmail, logout } = initialize(
+ process.env.API_KEY || ''
+,
+ yourAsyncJWTGeneratorMethod
+ );
+ const { request, pauseMessageStream, resumeMessageStream } = getInAppMessages(
+ {
+ // Here, you can configure the SDK. For more information, check out the
+ // web SDK's GitHub repository: https://github.com/iterable/iterable-web-sdk
+ count: 20,
+ displayInterval: 1000,
+ onOpenScreenReaderMessage: '...',
+ onOpenNodeToTakeFocus: 'input',
+ packageName: '<Unique identifier for your website integration in Iterable>',
+ rightOffset: '20px',
+ topOffset: '20px',
+ bottomOffset: '20px',
+ handleLinks: 'external-new-tab'
+ },
+ true
+ );
+ // Set the email address, and make the request to get the in-app messages
+ // that are available for that user.
+ setEmail('docs@iterable.com').then(() => {
+ request();
+ });
+})();
