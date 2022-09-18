f8a85a50e108ea2abb182d387e5ac0f514abe9e1
yashasvibajpai <yashasvi.bajpai1999@gmail.com>
2022-09-19 04:47:27 +0530

    tryout unpkg cdn

--- js/jwtfunc.js
+++ js/jwtfunc.js
@@ -32,4 +32,4 @@ function generateJWTToken(header, data, secret) {
   const signature = base64url(CryptoJS.HmacSHA256(token, secret));
   return `${token}.${signature}`;
 }
-exports.generateJWTToken = generateJWTToken;
+export { generateJWTToken };
