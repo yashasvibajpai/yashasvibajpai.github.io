13b687c904c160acb4ea65c913d00f39a93d3ada
yashasvibajpai <yashasvi.bajpai1999@gmail.com>
2022-09-19 05:23:01 +0530

    fixing crypto import

--- js/jwtfunc.js
+++ js/jwtfunc.js
@@ -1,8 +1,9 @@
-import CryptoJS from "../node_modules/crypto-js/index.js";
+// import from "";
+import { Base64, Utf8, HmacSHA256 } from "crypto-js";
 
 function base64url(source) {
   // Encode in classical base64
-  let encodedSource = CryptoJS.enc.Base64.stringify(source);
+  let encodedSource = Base64.stringify(source);
 
   // Remove padding equal characters
   encodedSource = encodedSource.replace(/=+$/, "");
@@ -24,12 +25,12 @@ function base64url(source) {
  * */
  
 function generateJWTToken(header, data, secret) {
-  const stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
+  const stringifiedHeader = Utf8.parse(JSON.stringify(header));
   const encodedHeader = base64url(stringifiedHeader);
-  const stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data));
+  const stringifiedData = Utf8.parse(JSON.stringify(data));
   const encodedData = base64url(stringifiedData);
   const token = `${encodedHeader}.${encodedData}`;
-  const signature = base64url(CryptoJS.HmacSHA256(token, secret));
+  const signature = base64url(HmacSHA256(token, secret));
   return `${token}.${signature}`;
 }
 export { generateJWTToken };
